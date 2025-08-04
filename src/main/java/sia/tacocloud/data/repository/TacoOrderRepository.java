bac9bf48f1e036491e3e89cecf738e77249f8352
pushkart egor <pushkartegor@gmail.com>
2025-08-04 14:04:59 +0300

    add-cassandra

--- src/main/java/sia/tacocloud/data/repository/TacoOrderRepository.java
+++ src/main/java/sia/tacocloud/data/repository/TacoOrderRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.data.model.TacoOrder;
 
 import java.util.List;
+import java.util.UUID;
 
-public interface TacoOrderRepository extends CrudRepository<TacoOrder, Long> {
+public interface TacoOrderRepository extends CrudRepository<TacoOrder, UUID> {
 
     List<TacoOrder> findByDeliveryZip(String deliveryZip);
 
